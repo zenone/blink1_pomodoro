54c658ed255da74bf13bfb52d4a5ceed19d89dab
steve <steve@zenone.org>
2018-06-01 15:04:02 -0700

    Added red/blue flash between pomodoro transitions

--- blink1_pomodoro.py
+++ blink1_pomodoro.py
@@ -54,16 +54,19 @@ def pomodoro():
             # Work / Focus Session
             status(set=set, rep=rep, msg='Work ({0} mins)'.format(TIME_WORK))
             use_blink(color='red', minutes=TIME_WORK)
+            flash_blink()
 
             # Short Rest
             if set < TIME_SETS and rep + 1 < TIME_REPS:
                 status(set=set, rep=rep, msg='Rest ({0} mins)'.format(TIME_REST))
                 use_blink(color='yellow', minutes=TIME_REST)
+                flash_blink()
 
         # Long(er) Break
         if set + 1 < TIME_SETS:
             status(set=set, rep=rep, msg='Break ({0} mins)'.format(TIME_BREAK))
             use_blink(color='green', minutes=TIME_BREAK)
+            flash_blink()
 
 
 def use_blink(color, minutes):
@@ -78,15 +81,49 @@ def use_blink(color, minutes):
             b1.fade_to_color(1000, color)
             time.sleep(minutes * 60)
     except KeyboardInterrupt:
-        logging.info('Exiting. Control-C pressed.')
-        b1.fade_to_rgb(1000, 0, 0, 0)
-        b1.close()
-        sys.exit(1)
+        control_c(b1)
     except Exception as e:
-        logging.info('Error: {0}'.format(e))
-        logging.info('Make sure the blink(1) device is plugged in.')
-        logging.info('Exiting.')
-        sys.exit(1)
+        no_device_found(e)
+
+
+def flash_blink():
+    """
+    Flash the blink(1) between blue and red.
+    """
+    try:
+        with blink1() as b1:
+            b1.fade_to_rgb(.5, 0, 0, 0)
+            for i in xrange(5):
+                b1.fade_to_color(100, 'blue')
+                time.sleep(.1)
+                b1.fade_to_color(100, 'red')
+                time.sleep(.1)
+            b1.fade_to_rgb(.5, 0, 0, 0)
+    except KeyboardInterrupt:
+        control_c(b1)
+    except Exception as e:
+        no_device_found(e)
+
+
+def control_c(b1):
+    """
+    Turn off blink(1) when contrl-c is pressed.
+    :param b1: blink1 function
+    """
+    logging.info('Exiting. Control-C pressed.')
+    b1.fade_to_rgb(1000, 0, 0, 0)
+    b1.close()
+    sys.exit(1)
+
+
+def no_device_found(e):
+    """
+    Inform user if blink(1) device is not found.
+    """
+    logging.info('Error: {0}'.format(e))
+    logging.info('Make sure the blink(1) device is plugged in.')
+    logging.info('Exiting.')
+    sys.exit(1)
 
 
 def main():
